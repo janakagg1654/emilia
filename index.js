1caa9605b294937c88e1425c76217f2a5c301c5c
ᴄyʙᴇʀ_ᴅᴀʀᴋ <89356302+CYBER-DARK-YT@users.noreply.github.com>
2022-06-30 00:32:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const client = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const start = async () => {
 	try {
-		console.log(`levanter ${VERSION}`)
+		console.log(`Dark bot ${VERSION}`)
 		await DATABASE.sync()
 		console.log('DB syncing')
 		await client.connect()
