f7cfc6880956803c0bfb979a068f576138e8ec20
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-19 09:18:05 -0700

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -41,43 +41,6 @@ bot(
 	}
 )
 
-bot(
-	{
-		pattern: 'add ?(.*)',
-		fromMe: true,
-		desc: 'To add members',
-		type: 'group',
-		onlyGroup: true,
-	},
-	async (message, match) => {
-		const participants = await message.groupMetadata(message.jid)
-		const isImAdmin = await isAdmin(participants, message.client.user.jid)
-		if (!isImAdmin) return await message.send(`_I'm not admin._`)
-		match = match || message.reply_message.jid
-		if (!match) return await message.send('Example : add 91987654321')
-		// if (!match.startsWith('@@')) {
-		// 	match = jidToNum(match)
-		// 	const button = await genButtonMessage(
-		// 		[
-		// 			{ id: `@@`, text: 'NO' },
-		// 			{ id: `add @@${match}`, text: 'YES' },
-		// 		],
-		// 		`Your Number maybe banned, Do you want add @${match}`,
-		// 		''
-		// 	)
-		// 	return await message.send(
-		// 		button,
-		// 		{ contextInfo: { mentionedJid: [numToJid(match)] } },
-		// 		'button'
-		// 	)
-		// }
-		match = jidToNum(match)
-		const res = await message.Add(match)
-		if (res == '403') return await message.send('_Failed, Invite sent_')
-		else if (res && res != '200')
-			return await message.send(res, { quoted: message.data })
-	}
-)
 
 bot(
 	{
