6900dab2099f343c55542eeb86870bb7c42fb055
Dumebi18 <156431170+Dumebi18@users.noreply.github.com>
2024-03-08 21:35:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '9_3_2062_b394_74fb').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
